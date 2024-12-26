d148a94af4eebf6b31549fde8c5b85784f94428d
kamesan1577 <kamesan1577@users.noreply.github.com>
2024-12-26 14:03:16 +0000

    hoge

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ DOT_FILES=($(ls -A))
 for file in ${DOT_FILES[@]}
 do
   if [[ $file == .* ]]; then
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -sf $HOME/dotfiles/$file $HOME/$file
   fi
 done
